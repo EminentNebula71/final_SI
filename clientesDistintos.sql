56b75849b756697fd3ab6adceb9545eff22f727a
Angel Bernal Garcia <you@example.com>
2019-12-16 18:15:54 +0100

    hechos algunos yo creo

---
+++ clientesDistintos.sql
@@ -0,0 +1,38 @@
+﻿select count(num)from (select count(*)as num
+from orders
+where totalamount >=100
+and extract (year from orderdate)=2010 and extract (month from orderdate) =4
+group by extract (year from orderdate),extract (month from orderdate) ,customerid) as a;
+
+explain select count(num)from (select count(*)as num
+from orders
+where totalamount >=100
+and extract (year from orderdate)=2010 and extract (month from orderdate) =4
+group by extract (year from orderdate),extract (month from orderdate) ,customerid) as a;
+
+drop index idx_totalamount;
+create index idx_totalamount on orders(totalamount);
+
+explain select count(num)from (select count(*)as num
+from orders
+where totalamount >=100
+and extract (year from orderdate)=2010 and extract (month from orderdate) =4
+group by extract (year from orderdate),extract (month from orderdate) ,customerid) as a;
+
+drop index idx_customerid;
+create index idx_customerid on orders(customerid);
+
+explain select count(num)from (select count(*)as num
+from orders
+where totalamount >=100
+and extract (year from orderdate)=2010 and extract (month from orderdate) =4
+group by extract (year from orderdate),extract (month from orderdate) ,customerid) as a;
+
+drop index idx_orderdate;
+create index idx_orderdate on orders(orderdate);
+
+explain select count(num)from (select count(*)as num
+from orders
+where totalamount >=100
+and extract (year from orderdate)=2010 and extract (month from orderdate) =4
+group by extract (year from orderdate),extract (month from orderdate) ,customerid) as a;
---
+++ countStatus.sql
@@ -0,0 +1,28 @@
+﻿--b) 
+explain select count(*) from orders where status is null;
+
+explain select count(*) from orders where status ='Shipped';
+
+--c)
+DROP INDEX idx_status;
+CREATE INDEX idx_status on orders(status);
+
+--d) 
+explain select count(*) from orders where status ='Shipped';
+
+explain select count(*) from orders where status is null;
+
+
+--e)
+
+ANALYZE orders;
+
+--f)
+explain select count(*) from orders where status ='Shipped';
+
+explain select count(*) from orders where status is null;
+
+
+--g)
+explain select count(*) from orders where status ='Paid';
+explain select count(*) from orders where status ='Processed';
